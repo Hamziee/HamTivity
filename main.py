86b82e8f48ee0d4cebd12c1a1c2fb1e9c92820df
Hamza <hamza@heo-systems.com>
2024-10-15 21:03:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 scriptMain = "Main"
-versionMain = "v0.2.0"
+versionMain = "v0.2.1"
 
 import threading
 from LastFM import loop
 import time
+import config
 
-def logFormat():
-    return f"{time.strftime("[%H:%M:%S - %d/%m/%Y]")} | {scriptMain} {versionMain}"
+if __name__ == "__main__":
+    def logFormat():
+        return f"{time.strftime("[%H:%M:%S - %d/%m/%Y]")} | {scriptMain} {versionMain}"
 
-print(f"{logFormat()} Using LastFM for activity data.")
-loop_thread = threading.Thread(target=loop)
-loop_thread.start()
+    if config.activityMethod.lower() == "lastfm":
+        print(f"{logFormat()} Using LastFM for activity data.")
+        loop_thread = threading.Thread(target=loop)
+        loop_thread.start()
